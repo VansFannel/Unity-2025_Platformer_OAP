88c07a66b56d9daabdf7a82750cd199085ccb9a5
VansFannel <liberado27@hotmail.com>
2025-12-18 19:14:27 +0100

    First commit. Added some tile maps, character, some logic, etc.

---
+++ Assets/Platformer/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerController controller; 
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
